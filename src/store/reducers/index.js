698ac238cfb601a95f6eb16a0ae3c022d2248306
Enjay Tarigan <tariganagc@gmail.com>
2021-11-15 15:51:22 +0700

    added topics entity

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { loginReducer } from "./loginReducer";
 import { registerReducer } from "./registerReducer";
+import { topicReducer } from "./topicReducer";
 
 export default reducer = combineReducers({
   login: loginReducer,
   register: registerReducer,
+  topic: topicReducer,
 });

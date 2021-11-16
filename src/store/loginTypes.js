ec62506a565d5dd421116a936b8b3b4aac155764
Enjay Tarigan <tariganagc@gmail.com>
2021-11-16 11:42:57 +0700

    styling home client

--- src/store/loginTypes.js
+++ src/store/loginTypes.js
@@ -1,3 +1,4 @@
 export const SET_LOGIN_ERROR = "login/Error";
 export const SET_LOGIN_LOADING = "login/Loading";
 export const SET_LOGIN_STATUS = "login/isLoggedIn";
+export const SET_LOGIN_USER = "login/userLoggedIn";
